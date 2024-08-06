4a5ba2e2b9bf282c6cec9fd8e7f277093e2fb8b6
Bikash-bluebash <bikash.choudhury@bluebash.co>
2024-08-06 18:26:27 +0530

    Added RSA-encryption logic for abdm

---
+++ lib/abdm/rsa_encryption.rb
@@ -0,0 +1,27 @@
+require 'openssl'
+require 'base64'
+
+module Abdm
+  class RsaEncryption
+    PUBLIC_KEYS = {
+      v1_v2_v3: "MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAstWB95C5pHLXiYW59qyO4Xb+59KYVm9Hywbo77qETZVAyc6VIsxU+UWhd/k/YtjZibCznB+HaXWX9TVTFs9Nwgv7LRGq5uLczpZQDrU7dnGkl/urRA8p0Jv/f8T0MZdFWQgks91uFffeBmJOb58u68ZRxSYGMPe4hb9XXKDVsgoSJaRNYviH7RgAI2QhTCwLEiMqIaUX3p1SAc178ZlN8qHXSSGXvhDR1GKM+y2DIyJqlzfik7lD14mDY/I4lcbftib8cv7llkybtjX1AayfZp4XpmIXKWv8nRM488/jOAF81Bi13paKgpjQUUuwq9tb5Qd/DChytYgBTBTJFe7irDFCmTIcqPr8+IMB7tXA3YXPp3z605Z6cGoYxezUm2Nz2o6oUmarDUntDhq/PnkNergmSeSvS8gD9DHBuJkJWZweG3xOPXiKQAUBr92mdFhJGm6fitO5jsBxgpmulxpG0oKDy9lAOLWSqK92JMcbMNHn4wRikdI9HSiXrrI7fLhJYTbyU3I4v5ESdEsayHXuiwO/1C8y56egzKSw44GAtEpbAkTNEEfK5H5R0QnVBIXOvfeF4tzGvmkfOO6nNXU3o/WAdOyV3xSQ9dqLY5MEL4sJCGY1iJBIAQ452s8v0ynJG5Yq+8hNhsCVnklCzAlsIzQpnSVDUVEzv17grVAw078CAwEAAQ==",
+      phr_v1: "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA7Zq7YKcjmccSBnR9CDHd6IX96V7D/a2XSMs+yCgejSe956mqjA/0Q9h+Xnx7ZZdwe2Tf2Jq/mWXa+gYdnta58otreXg/5oGnNV3Edlixz1Oc8tJg5bG4sIUCGZcbEQGSbm1iC+Fp1kS+YLVG4Su8KoRxcCvRJI2QkfqAruX3JoFjggOkv0TgWCo9z6NV6PPmPN3UsXyH3OPDi3Ewnvd64ngCUKPSBiIDwhLj2yYSShcxH8aWbrz00SJodBJzqgjvCfZuljBXXIN4Ngi/nzqEJ7woKQ1kNgWoHFZy7YL74PihW//4OlniSRoITX+7ChILIv2ezSmAdIjpNJ9DghILIv2ezSmAdIjpNJ9Dg9XKcQIDAQAB"
+    }.freeze
+
+    def initialize(api_version = :v1_v2_v3)
+      @public_key = load_public_key(PUBLIC_KEYS[api_version])
+    end
+
+    def encrypt(data)
+      encrypted_data = @public_key.public_encrypt(data.b, OpenSSL::PKey::RSA::PKCS1_OAEP_PADDING)
+      Base64.strict_encode64(encrypted_data)
+    end
+
+    private
+
+    def load_public_key(public_key_pem)
+      public_key_der = Base64.decode64(public_key_pem)
+      OpenSSL::PKey::RSA.new(public_key_der)
+    end
+  end
+end
