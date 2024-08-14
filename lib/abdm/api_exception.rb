dd16b54685e47329685c40b45b91fbcf5d51d765
Bikash-bluebash <bikash.choudhury@bluebash.co>
2024-08-14 17:47:56 +0530

    Handle ABDM api exceptions

---
+++ lib/abdm/api_exception.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module Abdm
+  class APIException < StandardError
+    attr_accessor :response_body, :response_code
+
+    def initialize(response)
+      @response_body = response.parsed_response
+      @response_code = response.code
+
+      super error_message
+    end
+
+    private
+
+    def error_message
+      response_body.dig('error', 'message')
+    end
+  end
+end
