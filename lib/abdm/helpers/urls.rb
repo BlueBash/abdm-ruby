afaff7915289864c275629432a65804deaa16312
Bikash-bluebash <bikash.choudhury@bluebash.co>
2024-08-14 17:48:44 +0530

    Added common files for ABDM api base_urls

---
+++ lib/abdm/helpers/urls.rb
@@ -0,0 +1,8 @@
+
+if AbdmConfig[:environment] == 'sandbox'
+  ABHA_BASE_URL = 'https://abhasbx.abdm.gov.in/'.freeze
+  GATEWAY_URL = 'https://dev.abdm.gov.in/gateway/v0.5/sessions'.freeze
+else
+  ABHA_BASE_URL = ''.freeze
+  GATEWAY_URL = ''.freeze
+end
