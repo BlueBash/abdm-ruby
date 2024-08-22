a065290e419f4bd42d8a0f743c8f303d3120050d
Bikash-bluebash <bikash.choudhury@bluebash.co>
2024-08-22 18:30:54 +0530

    refactor the code for session handler

--- lib/abdm/helpers/session_handler.rb
+++ lib/abdm/helpers/session_handler.rb
@@ -2,8 +2,8 @@ module Abdm
   module Helpers
     module SessionHandler
 
-      def authenticate
-        return if session_token_valid?
+      def authentication_token
+        return session_token if session_token && session_token_valid?
 
         request_session_token
       end
@@ -16,10 +16,10 @@ module Abdm
 
       def request_session_token
         @token_expiry_time = Time.now + 5.minutes
-        response = post(GATEWAY_URL, session_credentials_body)
+        response           = post(GATEWAY_URL, session_credentials_body)
 
-        validate_response(response)
-        store_session_token(response)
+        render_response(response, store_identifiers: 'session_token')
+        session_token
       end
 
       def session_credentials_body
@@ -28,12 +28,9 @@ module Abdm
             clientId: AbdmConfig[:client_id],
             clientSecret: AbdmConfig[:client_secret],
             grantType: 'client_credentials'
-          }.to_json
+          }.to_json,
+          headers: content_type
         }
-      end
-
-      def store_session_token(response)
-        @session_token = response.parsed_response['accessToken']
       end
     end
   end
