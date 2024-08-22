e9f23a51639881f8bf2ddbb90885af2488e70a59
Bikash-bluebash <bikash.choudhury@bluebash.co>
2024-08-22 18:51:44 +0530

    Update the base file with updated code

--- lib/abdm/base.rb
+++ lib/abdm/base.rb
@@ -1,27 +1,26 @@
 require 'json'
 require 'time'
 
-require 'abdm/api_exception'
+require 'abdm/helpers/headers_params'
 require 'abdm/helpers/api_operations'
 require 'abdm/helpers/session_handler'
+require 'abdm/helpers/response_handler'
+require 'abdm/helpers/store_resources'
 
 module Abdm
   class Base < OpenStruct
-    attr_accessor :session_token, :token_expiry_time
+    attr_accessor :session_token, :token_expiry_time, :transaction_id, :parsed_response, :x_token, :t_token
 
     extend HTTParty
+    include Abdm::Helpers::HeadersParams
     include Abdm::Helpers::ApiOperations
     include Abdm::Helpers::SessionHandler
+    include Abdm::Helpers::ResponseHandler
+    include Abdm::Helpers::StoreResources
 
     def initialize
       require 'abdm/helpers/urls'
       super
-    end
-
-    def validate_response(response)
-      unless response.code.between?(200, 208)
-        raise Abdm::APIException.new response
-      end
     end
   end
 end
