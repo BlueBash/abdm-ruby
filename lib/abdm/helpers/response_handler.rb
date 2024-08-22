8d2f6703872d37bdf9eb520c8ad4c516631f0bad
Bikash-bluebash <bikash.choudhury@bluebash.co>
2024-08-22 18:39:44 +0530

    Added response handler file for verify error and parsed the response

---
+++ lib/abdm/helpers/response_handler.rb
@@ -0,0 +1,24 @@
+module Abdm
+  module Helpers
+    module ResponseHandler
+
+      def render_response(res, store_identifiers: nil)
+        validate_response(res)
+
+        store_identifiers.split(', ').each do |store_identifier|
+          public_send("store_#{store_identifier}")
+        end if store_identifiers.present?
+
+        parsed_response
+      end
+
+      private
+
+      def validate_response(res)
+        unless res.code.between?(200, 208)
+          raise Abdm::APIException.new res
+        end
+      end
+    end
+  end
+end
