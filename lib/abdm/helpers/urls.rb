3e45250ba4cd88c06b6033bd8c42d8199e8bc4f1
Bikash-bluebash <bikash.choudhury@bluebash.co>
2024-08-22 18:50:58 +0530

    Update the urls file
    -> This file now work dynamically both environment, such as `production`
    and `sandbox`

--- lib/abdm/helpers/urls.rb
+++ lib/abdm/helpers/urls.rb
@@ -1,8 +1,8 @@
 
 if AbdmConfig[:environment] == 'sandbox'
-  ABHA_BASE_URL = 'https://abhasbx.abdm.gov.in/'.freeze
+  ABHA_BASE_URL = 'https://abhasbx.abdm.gov.in'.freeze
   GATEWAY_URL = 'https://dev.abdm.gov.in/gateway/v0.5/sessions'.freeze
 else
-  ABHA_BASE_URL = ''.freeze
+  ABHA_BASE_URL = 'https://abha.abdm.gov.in'.freeze
   GATEWAY_URL = ''.freeze
 end
