74e4b2297952c0a37b1a9b4e025a61635e22cb89
Bikash-bluebash <bikash.choudhury@bluebash.co>
2024-09-10 16:09:09 +0530

    Update the template for hpr_id and password

--- lib/generators/abdm/templates/initializer.rb
+++ lib/generators/abdm/templates/initializer.rb
@@ -1,11 +1,17 @@
 # Configuration for ABDM integration
 #
-# You need to set the client_id and client_secret and environment.
+# You need to set the client_id, client_secret, environment, hpr_id, and password.
 # By default, the environment is set to 'production'. You can change it to 'sandbox'.
+#
+# The hpr_id and password are used to automatically generate the hpr_token
+# required for certain API requests, so you don't need to manually handle token generation.
 #
 # Example:
 # ABDMConfig = {
 #   environment: 'sandbox', # or 'production'
 #   client_id: 'your_client_id',
-#   client_secret: 'your_client_secret'
+#   client_secret: 'your_client_secret',
+#
+#   hpr_id: 'your_hpr_id Ex: 71-2123-1232-4354',
+#   password: 'your_password'
 # }
