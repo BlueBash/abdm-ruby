018bd4f01b84a8da337d9ae9cda9d0cd57ee5bb7
Bikash-bluebash <bikash.choudhury@bluebash.co>
2024-09-10 15:59:44 +0530

    make common dry method for send api request

--- lib/abdm/common_base.rb
+++ lib/abdm/common_base.rb
@@ -13,6 +13,24 @@ module ABDM
       render_response(res, store_identifiers:)
     end
 
+    def send_master_api_request(klass, method_name, params: {}, api_method: :post, search_params: {})
+      method_name = method_name.to_s.remove('fetch_')
+      base_url    = eval("#{klass}_BASE_URL")
+      path        = MASTERAPI_PATH[method_name]
+      url         = "#{base_url}/#{path}"
+
+      url         = apply_search_params(url, search_params) if search_params.present?
+
+      params      = {
+        body: request_body_params(method_name, params),
+        headers: headers_params(klass)
+      }
+
+      res = send(api_method, url, params)
+
+      render_response(res)
+    end
+
     private
 
     def request_body_params(method_name, params)
@@ -20,6 +38,16 @@ module ABDM
       params = public_send("params_for_#{method_name}", **params)
 
       params.is_a?(Hash) ? params.compact.to_json : params
+    end
+
+    def apply_search_params(url, search_params)
+      return url if search_params.empty?
+
+      search_params.each do |key, value|
+        url += url.include?('?') ? "&#{key}=#{value}" : "?#{key}=#{value}"
+      end
+
+      url
     end
   end
 end
